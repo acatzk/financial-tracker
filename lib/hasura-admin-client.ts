4c7c1432469af043f69362c29128f1d25c081aea
acatzk <joshuaimalay@gmail.com>
2022-04-25 22:26:06 +0800

    chore(deps): add @types/node deps

--- lib/hasura-admin-client.ts
+++
@@ -1,7 +0,0 @@
-import { GraphQLClient } from 'graphql-request'
-
-export const hasuraAdminClient = new GraphQLClient(`${process.env.HASURA_GRAPHQL_API_ENDPOINT}`, {
-  headers: {
-    'x-hasura-admin-secret': `${process.env.HASURA_GRAPHQL_ADMIN_SECRET}`
-  }
-})
