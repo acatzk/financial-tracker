dcf62ef7a11c0c51fa51d9c139e65217dbf762c6
acatzk <joshuaimalay@gmail.com>
2022-04-25 14:21:50 +0800

    fix: add login and signup on sigle page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,28 @@
 import { NextPage } from 'next'
 import DefaultLayout from 'layouts/DefaultLayout'
-import Image from 'next/image'
-import Link from 'next/link'
 import { classNames } from 'utils'
 import LoginWithButtons from 'components/LoginWithButtons'
+import { useState } from 'react'
+import Image from 'next/image'
 
 const Index: NextPage = () => {
+  let [isLoginPage, setIsLoginPage] = useState(true)
+  const image_banner =
+    'https://images.unsplash.com/photo-1434626881859-194d67b2b86f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80'
+
+  const handleAuthSwitchForm = () => setIsLoginPage((isLoginPage = !isLoginPage))
+
   return (
     <DefaultLayout metaHead="Sign in">
       <div className="relative bg-white overflow-hidden">
         <div className="flex min-h-screen">
-          <div className="relative bg-green-200 w-0 md:w-1/2">
+          <div className="relative w-0 md:w-1/2">
             <Image
-              src="https://images.unsplash.com/photo-1434626881859-194d67b2b86f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80"
+              src={image_banner}
               layout="fill"
               objectFit="cover"
+              blurDataURL={image_banner}
+              placeholder="blur"
               alt=""
             />
           </div>
@@ -24,10 +32,23 @@ const Index: NextPage = () => {
               <div className="card bg-red-400 shadow-lg  w-full h-full rounded-3xl absolute  transform rotate-6"></div>
               <div className="relative w-full rounded-3xl  px-6 py-4 bg-gray-100 shadow-md">
                 <label className="block mt-3 text-sm text-gray-700 text-center font-semibold">
-                  Login
+                  {isLoginPage ? 'Login' : 'Sign Up'}
                 </label>
                 <form method="#" action="#" className="mt-10">
-                  <div>
+                  {!isLoginPage && (
+                    <div>
+                      <input
+                        type="text"
+                        placeholder="Enter Name"
+                        className={classNames(
+                          'mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg',
+                          'hover:bg-blue-100 focus:bg-blue-100 focus:ring-0 transition ease-in-out duration-150'
+                        )}
+                      />
+                    </div>
+                  )}
+
+                  <div className="mt-4">
                     <input
                       type="email"
                       placeholder="Enter Email"
@@ -38,7 +59,7 @@ const Index: NextPage = () => {
                     />
                   </div>
 
-                  <div className="mt-7">
+                  <div className="mt-4">
                     <input
                       type="password"
                       placeholder="Enter Password"
@@ -49,25 +70,27 @@ const Index: NextPage = () => {
                     />
                   </div>
 
-                  <div className="mt-7 flex">
-                    <label className="inline-flex items-center w-full cursor-pointer">
-                      <input
-                        type="checkbox"
-                        className={classNames(
-                          'rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring',
-                          'focus:ring-indigo-200 focus:ring-opacity-50'
-                        )}
-                        name="remember"
-                      />
-                      <span className="ml-2 text-sm text-gray-600">Remember me</span>
-                    </label>
+                  {isLoginPage && (
+                    <div className="mt-7 flex">
+                      <label className="inline-flex items-center w-full cursor-pointer">
+                        <input
+                          type="checkbox"
+                          className={classNames(
+                            'rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring',
+                            'focus:ring-indigo-200 focus:ring-opacity-50'
+                          )}
+                          name="remember"
+                        />
+                        <span className="ml-2 text-sm text-gray-600">Remember me</span>
+                      </label>
 
-                    <div className="w-full text-right">
-                      <a className="underline text-sm text-gray-600 hover:text-gray-900" href="#">
-                        Forgot password?
-                      </a>
+                      <div className="w-full text-right">
+                        <a className="underline text-sm text-gray-600 hover:text-gray-900" href="#">
+                          Forgot password?
+                        </a>
+                      </div>
                     </div>
-                  </div>
+                  )}
 
                   <div className="mt-7">
                     <button
@@ -75,7 +98,7 @@ const Index: NextPage = () => {
                         'bg-blue-500 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner',
                         'focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105'
                       )}>
-                      Login
+                      {isLoginPage ? 'Login' : 'Sign Up'}
                     </button>
                   </div>
 
@@ -91,12 +114,14 @@ const Index: NextPage = () => {
 
                   <div className="mt-7">
                     <div className="flex justify-center items-center">
-                      <label className="mr-2 text-sm">You don't have an account?</label>
-                      <Link href="/sign-up">
-                        <a className="text-sm text-blue-500 transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
-                          Sign up
-                        </a>
-                      </Link>
+                      <label className="mr-2 text-sm">
+                        {!isLoginPage ? 'Do you have an account?' : "You don't have an account?"}
+                      </label>
+                      <button
+                        onClick={handleAuthSwitchForm}
+                        className="text-sm text-blue-500 transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+                        {!isLoginPage ? 'Login' : 'Sign Up'}
+                      </button>
                     </div>
                   </div>
                 </form>
